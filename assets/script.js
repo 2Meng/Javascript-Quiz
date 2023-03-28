28c96844dcd3b54e099372c792418aee2f86b0fa
2Meng <tyan1401@gmail.com>
2023-03-28 17:57:43 -0500

    added high scores table + timer + other code... so much.. code..!

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,19 @@
 var startBtn = document.getElementById('startBtn');
 var restartBtn = document.getElementById('restartBtn');
 var timerText = document.getElementById('timer');
-var answerBtn = document.getElementById('answerBtn')
 var currentText = document.getElementById('textbox');
 var answerText = document.getElementById('answerbox');
+var correctIncorrect = document.getElementById('correctIncorrect');
+var saveScore = document.getElementById('highscoreBtn');
+var usersName = document.getElementById('username');
+var highScoreTable = document.getElementById('highscoreTable');
+var clearScoresBtn = document.getElementById('clearScores');
 
 var score = 0;
 var counter = 0;
-var username = '';
-var timer = 60;
-var highscores = [];
+var timerSec = 10;
+var timerStarted = false;
+var highScore = [];
 
 var questions = [
     {
@@ -64,17 +68,134 @@ var questions = [
     },
 ];
 
+// starts the quiz&timer & will move quiz forward -> 1 when answer buttons are pressed
 startBtn.addEventListener('click', startQuiz);
 function startQuiz(){
     currentText.innerHTML = questions[counter].question;
-    answerText.innerHTML = '';
     
+    if(!timerStarted){
+        timerStarted = true;
+        timer();
+    }
+    answerButtons ();
+    endQuiz();
+};
+
+// function for answer buttons to increment when questions answered.
+function answerButtons (){
+    // for each question in the answers property - create a interactive button
     for(var i = 0; i < questions[counter].answers.length; i++){
         var answerBtn = document.createElement('button');
         answerBtn.textContent = questions[counter].answers[i];
-        answerText.appendChild(answerBtn)
-        answerBtn.setAttribute('id', 'answerOptions')
+        answerText.appendChild(answerBtn);
+        answerBtn.setAttribute('id', 'answerOptions');
+    
+        // answer buttons if correct & incorrect events
+        answerBtn.addEventListener('click', function(event){
+            if (event.target.textContent === questions[counter].correct){
+                counter ++;
+                score ++;
+                timerSec += 5;
+                correctIncorrect.innerHTML = 'correct';
+                correctIncorrect.style.color = 'green';
+                console.log(score)
+            } else {
+                counter ++;
+                score --;
+                timerSec -= 5;
+                correctIncorrect.innerHTML = 'incorrect';
+                correctIncorrect.style.color = 'red';
+                console.log(counter)
+                console.log(score)
+            }
+            timeout()
+            answerText.innerHTML = '';
+            startQuiz()
+        })
     }
 };
 
-// answerBtn.addEventListener('click', answer);
+// end of quiz function
+function endQuiz(){
+    currentText.innerHTML = 'You reached the end of the quiz!'
+
+    
+    saveScore.addEventListener('click', function(event){
+        var userName = usersName.value;
+        var scoreBoard = {
+            name: userName,
+            score: score
+        };
+        highScore.push(scoreBoard);
+    
+        highScore.sort(function(a, b){
+            return b.score - a.score
+        })
+    
+        localStorage.setItem('scoreboard', JSON.stringify(scoreBoard));
+        localStorage.setItem('highscores', JSON.stringify(highScore));
+        displayScores();
+    });
+    
+    clearScoresBtn.addEventListener('click', function() {
+        // Clear the high scores from local storage
+        localStorage.removeItem('highscores');
+        localStorage.removeItem('scoreboard');
+        
+        // Clear the list element
+        highScoreTable.innerHTML = '';
+      });
+    
+    function displayScores(){
+        highScoreTable.innerHTML = '';
+    
+        
+        var highScores = JSON.parse(localStorage.getItem('highscores'));
+        if(highScores) {
+            for (var i = 0; i < highScores.length; i++){
+                var score = highScores[i];
+                var scoreItem = document.createElement('div');
+                scoreItem.textContent = score.name + ': ' + score.score;
+                highScoreTable.appendChild(scoreItem);
+            }
+        }
+    }
+    
+    window.onload = function() {
+        displayScores();
+    };
+};
+
+// function to prompt user to input username and then save into "highscores"
+
+
+
+/* displays the users score
+function userScore(){
+    
+}; */
+
+
+
+// times out the correctIncorrect text
+function timeout(){
+    setTimeout(function(){
+        correctIncorrect.style.display = 'none';
+    }, 500);
+    correctIncorrect.style.display = '';
+};
+
+// timer for quiz
+function timer(){
+    var interval = setInterval(function(){
+        timerSec --;
+        timerText.innerHTML = "Time: " + timerSec;
+
+        if(timerSec === 0 || timerSec < 0) {
+            clearInterval(interval);
+            timerText.innerHTML = '';
+            answerText.innerHTML = '';
+            currentText.innerHTML = 'Times up!';
+        }
+    },1000);
+};
